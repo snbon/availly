683090ab0bdafb8e60667ba722bbfb2336fa18bf
snbon <sweaniz@icloud.com>
2025-08-28 20:53:38 +0200

    remove docker

--- apps/web/docker/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-
-# Replace environment variables in the template
-envsubst < /usr/share/nginx/html/env.template.js > /usr/share/nginx/html/env.js
-
-# Start nginx
-exec "$@"
--- apps/web/docker/env.template.js
+++
@@ -1,8 +0,0 @@
-// Environment configuration template
-// This file will be processed by the entrypoint script
-window.ENV = {
-  VITE_API_URL: '${VITE_API_URL}',
-  VITE_APP_URL: '${VITE_APP_URL}',
-  VITE_PUBLIC_URL: '${VITE_PUBLIC_URL}',
-  NODE_ENV: 'production'
-};
